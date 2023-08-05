15f09e1e6d4842a12d2f4eeabaa5a8ea5d79f19f
Ianduha13 <ianduhamelhayes@gmail.com>
2023-08-05 17:49:18 -0300

    refactor hero-gears

--- components/Gear.jsx
+++ components/Gear.jsx
@@ -2,9 +2,9 @@ import { VscSettingsGear } from "react-icons/vsc"
 const Gear = ({ reverse }) => {
   return (
     <VscSettingsGear
-      color="#d1d5db"
-      className={reverse ? "gear-reverse" : "animate-spin-slow "}
-      size={"3em"}
+      color="#5BC0BE"
+      className={` ${reverse ? "gear-reverse" : "animate-spin-slow"} `}
+      size={"2.5em"}
     />
   )
 }
--- components/Hero.js
+++ components/Hero.js
@@ -3,8 +3,8 @@ import { AiOutlineDown } from "react-icons/ai"
 
 const Hero = () => {
   return (
-    <section className="h-screen w-full bg-[url('/laptop.jpeg')] bg-cover bg-no-repeat text-black">
-      <header className="flex h-full flex-col items-center justify-center gap-4 bg-gradient-to-br from-orange-500 to-transparent text-center">
+    <section className="z-20 h-screen w-full bg-[url('/laptop.jpeg')] bg-cover bg-fixed bg-no-repeat text-cyan-bright">
+      <header className="flex h-full flex-col items-center justify-center gap-4 bg-gradient-to-br from-[#0B132B] to-transparent bg-fixed text-center">
         <h3 className="animate-fade-up text-2xl font-medium animate-delay-75">
           En Devink
         </h3>
@@ -14,11 +14,11 @@ const Hero = () => {
         <h4 className="animate-fade-up text-xl animate-delay-75">
           Desarrollamos soluciones de IT para tu empresa.
         </h4>
-        <button className="animate-fade-up rounded-full bg-white px-8 py-2 text-xl font-medium text-black animate-delay-75">
+        <button className="animate-fade-up rounded-full border border-[#6fffe9] bg-[#0B132B] px-8 py-2 text-xl font-medium  text-[#6FFFE9] animate-delay-75">
           Contáctanos
         </button>
         <figure className="absolute bottom-4">
-          <AiOutlineDown color="#000" className="animate-bounce text-2xl" />
+          <AiOutlineDown color="#6fffe9" className="animate-bounce text-2xl" />
         </figure>
       </header>
     </section>
