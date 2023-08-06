667d1bcaf6f8b79986e1a64c1a3c592fd65df555
Ianduha13 <ianduhamelhayes@gmail.com>
2023-08-06 09:44:44 -0300

    Refactoring navbar for responsiveness

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,12 +1,21 @@
 import React from "react"
+import Link from "next/link"
 
 const Navbar = () => {
   return (
-    <nav className="sticky top-0 z-50 flex h-20 w-full items-center justify-between bg-blue-dark px-12 text-white">
-      <figure className="w-[140px] pt-2">
+    <nav className=" sticky top-0 z-50 flex h-20 w-full items-center justify-between bg-blue-dark px-12 text-white">
+      <figure className="hidden w-[140px] pt-2 md:inline-flex">
         <img src="/logo.svg" alt="logo" className="object-contain" />
       </figure>
-      <ul className="flex gap-4 text-xl">
+
+      <figure className="inline-flex w-[80px] pt-2 md:hidden">
+        <img
+          src="/logo-without-name.svg"
+          alt="logo"
+          className="object-contain"
+        />
+      </figure>
+      <ul className=" hidden gap-4 text-xl md:inline-flex">
         <li>
           <a href="/">Inicio</a>
         </li>
@@ -20,6 +29,13 @@ const Navbar = () => {
           <a href="/contact">Contacto</a>
         </li>
       </ul>
+      <Link
+        className={`mt-2 rounded-xl bg-cyan-bright px-8 py-2 text-center text-xl font-medium text-blue-dark shadow-lg transition-colors duration-300 md:hidden`}
+        href="https://usemotion.com/meet/ian-duhamel/devink?d=20"
+        target="_blank"
+      >
+        Contáctanos
+      </Link>
     </nav>
   )
 }
