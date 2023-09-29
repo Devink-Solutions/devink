659a22b97529381b8aaf0bf0be611c82d513996e
Ianduha13 <ianduhamelhayes@gmail.com>
2023-09-29 15:46:30 -0300

    fix: lint and phone

--- components/Footer1.jsx
+++ components/Footer1.jsx
@@ -5,10 +5,7 @@ import {
   FaPhoneAlt,
   FaGithub,
   FaWhatsapp,
-  FaRocket,
-  FaPinterest,
-  FaTwitch,
-  FaYoutube
+  FaRocket
 } from 'react-icons/fa'
 import Link from 'next/link'
 
@@ -25,25 +22,25 @@ const Footer1 = () => {
         </figure>
         <div>
           <div className='flex  flex-col md:flex-row w-full justify-center items-center pb-4'>
-          <h2 className="flex items-center space-x-2 text-2xl">Devink desde la web para el mundo</h2>
+            <h2 className="flex items-center space-x-2 text-2xl">Devink desde la web para el mundo</h2>
             <FaRocket className="text-cyan text-2xl mt-4 md:ml-6 md:text-4xl" />
-         
+
           </div>
           <div className="mt-4 flex flex-col gap-4 items-center md:flex-row justify-around">
             <span className="flex items-center space-x-4">
               <FaPhoneAlt />
-              <span>+5493415366234</span>
+              <span>+543513084848</span>
             </span>
             <span className="flex items-center space-x-4">
               <FaEnvelope />
               <a
-              className="hover:text-blue-400 transition-colors duration-200 font-semibold text-lg"
-              href="mailto:team@devink.dev"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              team@devink.dev
-            </a>
+                className="hover:text-blue-400 transition-colors duration-200 font-semibold text-lg"
+                href="mailto:team@devink.dev"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                team@devink.dev
+              </a>
             </span>
           </div>
           <div className="mt-8 flex flex-row justify-evenly  gap-5">
@@ -63,7 +60,7 @@ const Footer1 = () => {
           <li className="cursor-pointer hover:underline">Contact Us</li>
         </ul>
         <span className="mt-8 md:mt-0">
-          &copy; {new Date().getFullYear()} This website is made with{" "}
+          &copy; {new Date().getFullYear()} This website is made with{' '}
           <Link
             className="hover:text-cyan-400 bg-clip-text text-transparent bg-gradient-to-r from-blue-400 to-green-500 animate-gradient-x"
             href="https://www.devink.dev/"
