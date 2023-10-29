ad8f131de86387a41b7637727554f636e97fc660
Ianduha13 <ianduhamelhayes@gmail.com>
2023-10-29 00:23:42 -0300

    hotfix: wrong reverse changes

--- components/Blogs.jsx
+++ components/Blogs.jsx
@@ -1,19 +1,28 @@
+/* eslint-disable import/prefer-default-export */
 import BlogCard from './BlogCard'
 
 const fetchData = async () => {
-  const response = await fetch('https://devink-cms.onrender.com/api/blogs?populate=cover', { cache: 'no-cache' })
-  // if kevin want to revalidate data he has to use { cache: 'no-cache' } as a second parameter of the fetch
+  const response = await fetch('https://devink-cms.onrender.com/api/blogs?populate=cover', { next: { revalidate: 86400 } })
   const data = response.json()
   return data
 }
 
-export default async function Blogs() {
+export async function Blogs() {
   const blogsData = await fetchData()
 
+  // Si no hay blogs, retornar null
+  if (!blogsData.data || blogsData.data.length === 0) {
+    return null
+  }
+
   return (
-    <div className="z-20 flex w-full flex-col items-center justify-center bg-blue-dark px-2 py-8 md:px-6">
-      <h3 className="py-6 text-2xl font-bold text-cyan-bright">Nuestros Blogs</h3>
-      <div className="grid max-w-[1300px] grid-cols-1 gap-2 px-2 sm:grid-cols-2 sm:gap-4 lg:grid-cols-3 ">
+    <div
+      id="Blogs"
+      style={{ scrollMarginTop: '80px' }}
+      className="z-20 flex h-[60vh] w-full flex-col items-center justify-center bg-blue-dark"
+    >
+      <h3 className="mb-4 text-2xl font-semibold text-cyan-bright">Nuestros Blogs</h3>
+      <div className="grid max-w-[1300px] grid-cols-1 gap-10 sm:grid-cols-2 lg:grid-cols-3">
         {blogsData.data?.map((blog) => (
           <BlogCard blog={blog} key={blog.id} />
         ))}
