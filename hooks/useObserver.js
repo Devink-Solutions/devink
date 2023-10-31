6b69b092a676cc751ae5801cb3c85b8b4792275d
Dylan <dylanperaltasalerno@gmail.com>
2023-10-30 22:50:27 -0300

    fixes for eslint

--- hooks/useObserver.js
+++ hooks/useObserver.js
@@ -1,15 +1,16 @@
-import { useEffect, useState } from 'react'
+/* eslint-disable consistent-return */
+import { useEffect, useState } from "react";
 
 export default function useObserver(options) {
-  const [ref, setRef] = useState(null)
-  const [inView, setInView] = useState(false)
+  const [ref, setRef] = useState(null);
+  const [inView, setInView] = useState(false);
   useEffect(() => {
-    if (!ref) return
+    if (!ref) return;
     const observer = new IntersectionObserver(([entry]) => {
-      setInView(entry.isIntersecting)
-    }, options)
-    observer.observe(ref)
-    return () => observer.disconnect()
-  }, [ref, options])
-  return [setRef, inView]
+      setInView(entry.isIntersecting);
+    }, options);
+    observer.observe(ref);
+    return () => observer.disconnect();
+  }, [ref, options]);
+  return [setRef, inView];
 }
