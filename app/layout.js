dee93bbc1683112d0dc5a49bd1c4ac7091c740bc
Ianduha13 <ianduhamelhayes@gmail.com>
2023-12-01 23:21:12 -0300

    feat: improving metadata

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import { Red_Hat_Display as redHatDisplay } from 'next/font/google'
 import Navbar from '@/components/Navbar'
 import './globals.css'
@@ -6,8 +7,10 @@ import Footer from '@/components/Footer'
 const RedHatDisplay = redHatDisplay({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Devink',
-  description: 'Soluciones IT para tu empresa',
+  title: 'Devink - Soluciones IT Innovadoras para Empresas',
+  description: 'En Devink, ofrecemos soluciones IT personalizadas, análisis de datos avanzados, y desarrollo de aplicaciones móviles para impulsar el crecimiento empresarial.Descubre cómo nuestra tecnología puede transformar tu negocio.',
+  keywords: 'soluciones IT, analytics, desarrollo móvil, transformación digital, tecnología empresarial',
+  author: 'Devink Solutions',
 }
 
 export default function RootLayout({ children }) {
