6d1a1d652e4c56e62e2e304446acddc97ca48c59
Ianduha13 <ianduhamelhayes@gmail.com>
2023-12-02 14:30:01 -0300

    refactor:improving data fetching in the pages

--- app/page.js
+++ app/page.js
@@ -8,23 +8,20 @@ import OurExperience from '@/components/OurExperience'
 import OurServicesScreen from '@/components/OurServicesScreen'
 import Steps from '@/components/Steps'
 import ContactForm from '@/components/ContactForm'
-import { Blogs } from '@/components/Blogs'
+import Blogs from '@/components/Blogs'
 import Cover from '@/components/Cover/Cover'
 
 export default function Home() {
   const [blogs, setBlogs] = useState([])
 
   const getBlogs = async () => {
-    const res = await fetch(
-      'https://strapi-cms.devink.tech/api/blogs?populate[cover][populate][]=media',
-    )
-      .then((r) => r.json())
-      .catch((err) => console.log(err))
-    return setBlogs(res.data)
+    const res = await fetch('/api/blogs').then((r) => r.json()).catch((err) => console.log(err))
+    return setBlogs(res.data.data)
   }
   useEffect(() => {
     getBlogs()
   }, [])
+
   useEffect(() => {
     init(process.env.NEXT_PUBLIC_AMPLITUDE_KEY, {
       defaultTracking: true,
