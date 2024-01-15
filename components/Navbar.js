d56514c6ad373b3c722302a234c6352b637d055d
Ianduha13 <ianduhamelhayes@gmail.com>
2024-01-15 16:47:41 -0300

    hotfix:navbar bg and links padding

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,16 +1,16 @@
 /* eslint-disable tailwindcss/migration-from-tailwind-2 */
 
-'use client'
+"use client";
 
-import React from 'react'
-import Link from 'next/link'
-import Image from 'next/image'
-import { AiOutlineMenu } from 'react-icons/ai'
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { AiOutlineMenu } from "react-icons/ai";
 // import { CgProfile } from 'react-icons/cg'
-import useModal from '@/hooks/useModal'
+import useModal from "@/hooks/useModal";
 
 export default function Navbar() {
-  const { isOpen, toggleDropdown, ref } = useModal()
+  const { isOpen, toggleDropdown, ref } = useModal();
   return (
     <>
       <div ref={ref} className="sticky   top-0 z-50 flex md:hidden ">
@@ -29,11 +29,18 @@ export default function Navbar() {
             <AiOutlineMenu size={40} color="cyan" />
           </button>
         </nav>
-        <nav className={`fixed ${isOpen ? 'translate-x-0' : 'translate-x-[100%]'} right-0 top-[80px]
+        <nav
+          className={`fixed ${
+            isOpen ? "translate-x-0" : "translate-x-[100%]"
+          } right-0 top-[80px]
        z-[30] flex w-[200px] flex-col rounded-bl-2xl bg-blue-dark py-2 text-white brightness-125 transition-transform duration-300`}
         >
           <Link href="/#services">
-            <button type="button" className="flex w-full items-center px-4 py-2" onClick={toggleDropdown}>
+            <button
+              type="button"
+              className="flex w-full items-center px-4 py-2"
+              onClick={toggleDropdown}
+            >
               {/*
               <button type="button" aria-label="Admin" className="bg-primary-yellow mr-4 flex h-12 w-12 items-center justify-center rounded-full">
                 <CgProfile size={40} color="cyan" />
@@ -48,9 +55,12 @@ export default function Navbar() {
                 <CgProfile size={40} color="cyan" />
               </button>
               */}
-            <button type="button" className="flex w-full items-center px-4 py-2" onClick={toggleDropdown}>
+            <button
+              type="button"
+              className="flex w-full items-center px-4 py-2"
+              onClick={toggleDropdown}
+            >
               <span className="text-xl font-medium">Contacto</span>
-
             </button>
           </Link>
           <Link href="/#blogs">
@@ -59,22 +69,20 @@ export default function Navbar() {
                 <CgProfile size={40} color="cyan" />
               </button>
               */}
-            <button type="button" className="flex w-full items-center px-4 py-2" onClick={toggleDropdown}>
+            <button
+              type="button"
+              className="flex w-full items-center px-4 py-2"
+              onClick={toggleDropdown}
+            >
               <span className="text-xl font-medium">Blogs</span>
             </button>
           </Link>
         </nav>
       </div>
-      <nav className=" relative top-0 z-50 hidden h-20 w-full items-center justify-between bg-blue-dark  px-12 text-white md:flex">
+      <nav className=" relative top-0 z-50 hidden h-20 w-full items-center justify-between  px-12 text-white md:flex">
         {/* Logo grande para pantallas medianas y grandes */}
         <Link href="/" className="hidden  cursor-pointer pt-2 md:inline-flex">
-          <Image
-            src="/logo.svg"
-            alt="logo"
-            width={180}
-            height={180}
-            priority
-          />
+          <Image src="/logo.svg" alt="logo" width={180} height={180} priority />
         </Link>
 
         {/* Logo pequeño para pantallas pequeñas */}
@@ -88,25 +96,33 @@ export default function Navbar() {
           />
         </Link>
 
-        <ul className="flex w-full grow items-center justify-end gap-10 text-xl">
-
+        <ul className="flex w-full grow items-center justify-end gap-4 text-xl">
           <li>
-            <Link className=" text-lg font-medium sm:text-xl xl:text-2xl " href="/#services">
+            <Link
+              className=" p-2 text-lg font-medium sm:text-xl xl:text-2xl "
+              href="/#services"
+            >
               Servicios
             </Link>
           </li>
           <li>
-            <Link className=" text-lg font-medium sm:text-xl xl:text-2xl " href="/#contact">
+            <Link
+              className=" p-2 text-lg font-medium sm:text-xl xl:text-2xl "
+              href="/#contact"
+            >
               Contacto
             </Link>
           </li>
           <li>
-            <Link className="hidden text-lg font-medium sm:inline-flex sm:text-xl xl:text-2xl " href="/#blogs">
+            <Link
+              className="hidden p-2 text-lg font-medium sm:inline-flex sm:text-xl xl:text-2xl "
+              href="/#blogs"
+            >
               Blogs
             </Link>
           </li>
         </ul>
       </nav>
     </>
-  )
+  );
 }
