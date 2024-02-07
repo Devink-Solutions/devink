cd6e393fa75d6fe99fd22fe8ad11db1524226102
Ianduha13 <ianduhamelhayes@gmail.com>
2024-02-07 11:23:40 -0300

    feat:new cms connection

--- app/api/blogs/route.js
+++ app/api/blogs/route.js
@@ -1,11 +1,14 @@
 import { NextResponse } from 'next/server'
 
 export async function GET() {
-  const res = await fetch('https://strapi-cms.devink.tech/api/blogs?populate[cover][populate][]=media', {
-    headers: {
-      'Content-Type': 'application/json',
+  const res = await fetch(
+    'https://devink-cms.cap.devink.tech/api/blogs?populate[cover][populate][]=media',
+    {
+      headers: {
+        'Content-Type': 'application/json',
+      },
     },
-  })
+  )
   const data = await res.json()
 
   return NextResponse.json({ data })
