83f655582d077517137b562b856e0b411b4354e6
Ianduha13 <ianduhamelhayes@gmail.com>
2024-03-06 14:40:52 -0300

    fix: hotfix deploy fix

--- app/api/blogs/route.js
+++ app/api/blogs/route.js
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server'
 
 export async function GET() {
   const res = await fetch(
-    'https://devink-cms.cap.devink.tech/api/blogs?populate[cover][populate][]=media',
+    'https://devink-cms.cap.devink.site/api/blogs?populate[cover][populate][]=media',
     {
       headers: {
         'Content-Type': 'application/json',
