c1e95b43a0f902d93cff83b6b306bb01f1f16ca0
kevinduhamelhayes <kevinduhamelh@gmail.com>
2024-03-22 01:46:15 -0300

    fix/footer_navbar

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,25 +2,47 @@
 import {
   FaLinkedin,
   FaInstagram,
+  FaRocket,
+  FaHeart,
 } from 'react-icons/fa'
+import { IoIosMail } from 'react-icons/io'
 import Link from 'next/link'
 import Image from 'next/image'
 
 const Footer = () => (
-  <footer className="flex min-h-[180px] w-full items-center justify-center space-y-6 bg-bg-dark p-10 text-white">
-    <div className="flex w-full max-w-[1440px] justify-between space-y-5 md:space-y-0">
-      <figure className="">
+  <footer className="flex min-h-[180px] w-full flex-col items-center justify-center bg-bg-dark p-10 text-white">
+    <div className="flex w-full flex-col items-center justify-center gap-8 md:grid md:grid-cols-3">
+      <div className="flex flex-col items-center gap-6 ">
         <Image
-          width={178}
-          height={60}
+          width={200}
+          height={100}
           src="/logoFooter.png"
           alt="logo"
-          className="mt-4 w-32"
+          className="mt-4 w-40"
         />
-      </figure>
-      <div className="mt-8 flex flex-col items-end gap-2 ">
-        <h3>Redes Sociales:</h3>
-        <div className="flex gap-5  ">
+        <p className="flex gap-4 text-center text-sm font-bold">
+          Desde la web para el mundo
+          <FaRocket className="text-2xl text-cyan-bright" />
+        </p>
+      </div>
+
+      <div className=" flex flex-col items-center  ">
+        <div className="flex flex-col  items-center gap-3">
+          <h3 className="text-lg font-medium">Contáctanos:</h3>
+          <Link
+            href="mailto:correo@example.com"
+            className="flex items-center justify-center gap-4 text-base font-semibold transition-colors duration-200 hover:text-blue-400"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <IoIosMail />
+            info@devink.dev
+          </Link>
+        </div>
+      </div>
+      <div className="flex flex-col items-center justify-center gap-3">
+        <h3 className="text-lg font-medium">Síguenos:</h3>
+        <div className="flex  gap-5">
           <Link
             href="https://www.linkedin.com/company/devink-solutions/"
             className="text-2xl font-semibold transition-colors duration-200 hover:text-blue-400"
@@ -38,8 +60,21 @@ const Footer = () => (
             <FaInstagram />
           </Link>
         </div>
+
       </div>
     </div>
+
+    <hr className="my-10 w-full  max-w-[1440px] border border-slate-700 shadow-xl  shadow-white" />
+
+    <div className="mb-4 flex w-full items-center justify-center gap-1 text-white">
+      &#169; 2024, This website is made with
+      {' '}
+      <FaHeart color="red" />
+      {' '}
+      by
+      <span className="font-bold text-cyan-bright"> Devink</span>
+    </div>
+
   </footer>
 )
 
