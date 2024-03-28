d2692830b1b9933613d0475784acc72c1cd1dc93
Ianduha13 <ianduhamelhayes@gmail.com>
2024-03-28 17:26:34 -0300

    refactor: migrate project to ts

---
+++ FaqsAccordion.tsx
@@ -0,0 +1,61 @@
+'use client'
+
+import { useTranslations } from 'next-intl'
+import Image from 'next/image'
+import { useEffect, useState } from 'react'
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion'
+import { spanishFaqsData } from '@/data/spanishFaqsData'
+import { englishFaqsData } from '@/data/englishFaqsData'
+import { portugeseFaqsData } from '@/data/portugeseFaqsData'
+
+export function FaqsAccordion({ locale }: { locale: string }) {
+  const f = useTranslations('Faqs')
+  const [localeData, setLocaleData] = useState(spanishFaqsData)
+
+  useEffect(() => {
+    if (locale === 'es') {
+      setLocaleData(spanishFaqsData)
+    }
+    if (locale === 'en') {
+      setLocaleData(englishFaqsData)
+    }
+    if (locale === 'pt') {
+      setLocaleData(portugeseFaqsData)
+    }
+  }, [locale])
+
+  return (
+    <div className="relative flex w-full flex-col items-center justify-center bg-bg-dark">
+      <div className="mb-10">
+        <h2 className="text-center text-4xl font-semibold text-white">{f('accordion')}</h2>
+      </div>
+
+      <Accordion type="single" collapsible className="z-10 mx-auto w-[90%] max-w-6xl">
+        {localeData ? localeData.map((item) => (
+          <AccordionItem key={item.id} value={item.id.toString()} className="mb-2">
+            <AccordionTrigger
+              className="flex items-center justify-between rounded-md bg-blue-dark px-5 py-3 text-lg font-medium text-white hover:brightness-110"
+            >
+              {item.title}
+            </AccordionTrigger>
+            <AccordionContent className="rounded-b-md bg-bg-dark p-6 text-lg font-medium text-gray-300">
+              {item.content}
+            </AccordionContent>
+          </AccordionItem>
+        )) : null}
+      </Accordion>
+      <Image
+        src="/lightSoluciones.png"
+        width={200}
+        height={200}
+        alt="light"
+        className="absolute bottom-[-50%] left-0 h-full w-[300px] rotate-180"
+      />
+    </div>
+  )
+}
---
+++ spanishFaqsData.js
@@ -0,0 +1,28 @@
+const spanishFaqsData = [
+  {
+    id: 1,
+    title: '¿Qué tecnologías utilizan en el desarrollo de aplicaciones web?',
+    content: 'Nuestra empresa se especializa en tecnologías modernas de desarrollo web, incluyendo JavaScript, React, Next.js, Redux, Astro y Zustand para la gestión de estado. Utilizamos bases de datos SQL y NoSQL y aplicamos las mejores prácticas de UI/UX para asegurar aplicaciones eficientes, atractivas y funcionales.',
+  },
+  {
+    id: 2,
+    title: '¿Cómo aseguran la calidad y el rendimiento en los proyectos?',
+    content: 'Empleamos diversas técnicas y herramientas para garantizar la calidad y el rendimiento. Esto incluye revisiones de código, pruebas automatizadas, y el uso de Shadow CN para optimizar el rendimiento de las aplicaciones web. Nuestro enfoque se centra en la eficiencia, seguridad y escalabilidad de cada proyecto.',
+  },
+  {
+    id: 3,
+    title: '¿Ofrecen servicios personalizados para e-commerce?',
+    content: 'Sí, ofrecemos soluciones de comercio electrónico personalizadas, centrándonos en la creación de una experiencia de usuario excepcional. Utilizamos tecnologías como React y Next.js para construir interfaces dinámicas y funcionales, integrando sistemas de gestión de contenido y pasarelas de pago para proporcionar una solución e-commerce completa.',
+  },
+  {
+    id: 4,
+    title: '¿Cómo abordan la seguridad en el desarrollo de aplicaciones?',
+    content: 'La seguridad es una prioridad en cada fase de desarrollo. Implementamos prácticas como la codificación segura, análisis estático y dinámico de código, pruebas de penetración, y cumplimiento de normativas de seguridad de datos. Además, educamos a nuestro equipo regularmente sobre las últimas tendencias en seguridad cibernética para prevenir vulnerabilidades.',
+  },
+  {
+    id: 5,
+    title: '¿Qué procesos siguen para el diseño de interfaces de usuario?',
+    content: 'Nuestro proceso de diseño de interfaces se centra en la experiencia del usuario. Comenzamos con la investigación y la definición de los usuarios, seguido de wireframing y creación de prototipos. Utilizamos herramientas modernas y seguimos las últimas tendencias en diseño UI/UX para asegurar que nuestras interfaces sean intuitivas, accesibles y estéticamente agradables. La retroalimentación de los usuarios es clave en nuestro proceso iterativo.',
+  }]
+
+export { spanishFaqsData }
